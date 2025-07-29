62c43e5d3116b92a198115349f11a34a3d96593b
Renan Gervatosky <renan_gervatosky@hotmail.com>
2025-07-29 16:02:48 -0300

    Update references

---
+++ src/main/resources/references/DragonBlockC/JinRyuu/DragonBC/common/Items/BaseModItem.java
@@ -0,0 +1,11 @@
+package JinRyuu.DragonBC.common.Items;
+
+import net.minecraft.item.Item;
+
+public class BaseModItem extends Item {}
+
+
+/* Location:              D:\Projetos\Dragon-Block-Reborn\src\main\resources\references\DragonBlockC-v1.4.85.jar!\JinRyuu\DragonBC\common\Items\BaseModItem.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       1.1.3
+ */
---
+++ src/main/resources/references/JRMCore/JinRyuu/JRMCore/p/SAmh.java
@@ -0,0 +1,25 @@
+/*    */ package JinRyuu.JRMCore.p;
+/*    */ 
+/*    */ import cpw.mods.fml.common.network.simpleimpl.IMessage;
+/*    */ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+/*    */ import net.minecraft.entity.player.EntityPlayer;
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ public abstract class SAmh<T extends IMessage>
+/*    */   extends Amh<T>
+/*    */ {
+/*    */   public final IMessage handleClientMessage(EntityPlayer player, T message, MessageContext ctx) {
+/* 17 */     return null;
+/*    */   }
+/*    */ }
+
+
+/* Location:              D:\Projetos\Dragon-Block-Reborn\src\main\resources\references\.\JRMCore-v1.3.51.jar!\JinRyuu\JRMCore\p\SAmh.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       1.1.3
+ */
